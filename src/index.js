c0cba4b41a733ae3dcf3c5edb13be04e3944433f
Afton Slone <afton.slone@gmail.com>
2020-11-12 19:54:57 -0500

    Finished with 7 of 9 tasks

--- src/index.js
+++ src/index.js
@@ -1,66 +1,107 @@
-console.log('project is up')
+console.log("project is up");
 
 // 👉 TASK 1- Select the following elements from the DOM:
 
 // A- BUTTONS
-const launchButton = null
-const confirmButton = null
-const cancelButton = null
-
+const launchButton = document.querySelector("#launchButton");
+const confirmButton = document.querySelector("#confirmButton");
+const cancelButton = document.querySelector("#cancelButton");
 // B- MESSAGES
-const successMessage = null
-const failureMessage = null
+const successMessage = document.querySelector(".success");
+const failureMessage = document.querySelector(".failure");
 
 // C- MODAL
-const modal = null
-
+const modal = document.querySelector(".modal");
 
 // 👉 TASK 2- Demo handling click events on button#launchButton, using:
 //  A- HTML inside index.html
 //  B- The DOM's element.onclick attribute
+launchButton.onclick = function () {
+  //   console.log("old way");
+};
 //  C- element.addEventListener('click', callback)
+const eventListener =
+  ("click",
+  (e) => {
+    console.log(`
+  event type: ${e.type}
+  event target: ${e.target.nodeName}
+  timestamp: ${Math.floor(e.timeStamp / 1000)}
 
+  `);
+  });
 
+launchButton.addEventListener("click", eventListener);
 // 👉 TASK 3- Create a function that launches!
 // It should open the confirmation modal.
 // Add it as an event listener for click events on the launch button.
 
-
+const launch =
+  ("click",
+  (e) => {
+    document.querySelector(".modal").classList.remove("off");
+    document.querySelector(".success").classList.add("off");
+    document.querySelector(".failure").classList.add("off");
+  });
+launchButton.addEventListener("click", launch);
 // 👉 TASK 4- Create a function to confirm the launch.
 // It should close the modal and display a success report.
 // Add it as a listener for clicks on the confirmation button.
+const confirm =
+  ("click",
+  (e) => {
+    document.querySelector(".modal").classList.add("off");
+    document.querySelector(".success").classList.remove("off");
+  });
 
+confirmButton.addEventListener("click", confirm);
 
 // 👉 TASK 5- Create a function to cancel the launch.
 // It should close the modal and display a failure report.
 // Add it as a listener for clicks on the cancellation button.
 
+const cancel =
+  ("click",
+  (e) => {
+    document.querySelector(".modal").classList.add("off");
+    document.querySelector(".failure").classList.remove("off");
+  });
+
+cancelButton.addEventListener("click", cancel);
 
 // 👉 TASK 6- Create a function that closes the modal if
 // the user hits the Escape key on their keyboard.
 // Add it as an event listener for 'keydown' events on document.
 
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    modal.classList.add("off");
+  }
+});
 
 // 👉 TASK 7- Add to ALL ELEMENTS ON THE PAGE an event listener for click events.
 // It should console.log the target 🎯 of the event.
 // It should also console.log the CURRENT target 🧭 of the event.
 // Play with stopPropagation and stopImmediatePropagation.
-
+Array.from(document.all).forEach((elem) => {
+  elem.addEventListener("click", (e) => {
+    console.log(`
+    event type: ${e.type}
+    event target: ${e.target.nodeName}
+    current target: ${e.currentTarget.nodeName}
+    timestamp: ${Math.floor(e.timeStamp / 1000)}
+  
+    `);
+  });
+});
 
 // 👉 TASK 8- [STRETCH] Create helper functions to make the code
 // more readable in tasks 3, 4, 5, 6
-function openModal() {
+function openModal() {}
 
-}
+function closeModal() {}
 
-function closeModal() {
-
-}
-
-function killReports() {
-
-}
-
+function killReports() {}
 
 // 👉 TASK 9- [STRETCH] Using the mouseover event on 'document',
 // log to the console the X and Y coordinates
